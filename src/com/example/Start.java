337f1db6eca2e6eaf113809f6e3cfc4919e31316
MIkhaaa99 <70886962+MIkhaaa99@users.noreply.github.com>
2021-09-22 02:20:40 +0300

    Correct method decodeByFrequencyAnalysis 18/33

--- src/com/example/Start.java
+++ src/com/example/Start.java
@@ -2,18 +2,54 @@ package com.example;
 
 import java.io.*;
 import java.util.*;
-                     /////////////Chapter X////////////////
+                     /////////////Chapters I-X////////////////
 public class Start {
 
     public static final String ALPH = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
-    public static final Map<String, Double> mapFrequencyOfLetters = new HashMap<>(Map.ofEntries(Map.entry("а", 0.07998), Map.entry("б", 0.01592), Map.entry("в", 0.04533), Map.entry("г", 0.01687), Map.entry("д", 0.02977), Map.entry("е", 0.08483), Map.entry("ё", 0.00013), Map.entry("ж", 0.0094), Map.entry("з", 0.01641), Map.entry("и", 0.07367), Map.entry("й", 0.01208), Map.entry("к", 0.03486), Map.entry("л", 0.04343), Map.entry("м", 0.03203), Map.entry("н", 0.067), Map.entry("о", 0.10983), Map.entry("п", 0.02804), Map.entry("р", 0.04746), Map.entry("с", 0.05473), Map.entry("т", 0.06318), Map.entry("у", 0.02615), Map.entry("ф", 0.00267), Map.entry("х", 0.00966), Map.entry("ц", 0.00486), Map.entry("ч", 0.0145), Map.entry("ш", 0.00718), Map.entry("щ", 0.00361), Map.entry("ъ", 0.00037), Map.entry("ы", 0.01898), Map.entry("ь", 0.01735), Map.entry("э", 0.00331), Map.entry("ю", 0.00639), Map.entry("я", 0.02001)));
+    public static final Map<String, Double> mapFrequencyOfLetters = new LinkedHashMap<>();
 
-    public static Map<String, Integer> mapCountOfSymbol = new HashMap<>(Map.ofEntries(Map.entry("а", 0), Map.entry("б", 0), Map.entry("в", 0), Map.entry("г", 0), Map.entry("д", 0), Map.entry("е", 0), Map.entry("ё", 0), Map.entry("ж", 0), Map.entry("з", 0), Map.entry("и", 0), Map.entry("й", 0), Map.entry("к", 0), Map.entry("л", 0), Map.entry("м", 0), Map.entry("н", 0), Map.entry("о", 0), Map.entry("п", 0), Map.entry("р", 0), Map.entry("с", 0), Map.entry("т", 0), Map.entry("у", 0), Map.entry("ф", 0), Map.entry("х", 0), Map.entry("ц", 0), Map.entry("ч", 0), Map.entry("ш", 0), Map.entry("щ", 0), Map.entry("ъ", 0), Map.entry("ы", 0), Map.entry("ь", 0), Map.entry("э", 0), Map.entry("ю", 0), Map.entry("я", 0)));
+    public static Map<String, Integer> mapCountOfSymbol = new LinkedHashMap<>(Map.ofEntries(Map.entry("а", 0), Map.entry("б", 0), Map.entry("в", 0), Map.entry("г", 0), Map.entry("д", 0), Map.entry("е", 0), Map.entry("ё", 0), Map.entry("ж", 0), Map.entry("з", 0), Map.entry("и", 0), Map.entry("й", 0), Map.entry("к", 0), Map.entry("л", 0), Map.entry("м", 0), Map.entry("н", 0), Map.entry("о", 0), Map.entry("п", 0), Map.entry("р", 0), Map.entry("с", 0), Map.entry("т", 0), Map.entry("у", 0), Map.entry("ф", 0), Map.entry("х", 0), Map.entry("ц", 0), Map.entry("ч", 0), Map.entry("ш", 0), Map.entry("щ", 0), Map.entry("ъ", 0), Map.entry("ы", 0), Map.entry("ь", 0), Map.entry("э", 0), Map.entry("ю", 0), Map.entry("я", 0)));
 
-    public static  Map<String, String> mapReplacementLetter = new HashMap<>(Map.ofEntries(Map.entry("я", ""), Map.entry("в", ""), Map.entry("а", ""), Map.entry("к", ""), Map.entry("б", ""), Map.entry("г", ""), Map.entry("д", ""), Map.entry("е", ""), Map.entry("ё", ""), Map.entry("ж", ""), Map.entry("з", ""), Map.entry("и", ""), Map.entry("й", ""), Map.entry("л", ""), Map.entry("м", ""), Map.entry("н", ""), Map.entry("о", ""), Map.entry("п", ""), Map.entry("р", ""), Map.entry("с", ""), Map.entry("т", ""), Map.entry("у", ""), Map.entry("ф", ""), Map.entry("х", ""), Map.entry("ц", ""), Map.entry("ч", ""), Map.entry("ш", ""), Map.entry("щ", ""), Map.entry("ъ", ""), Map.entry("ы", ""), Map.entry("ь", ""), Map.entry("э", ""), Map.entry("ю", "")));
+    public static  Map<String, String> mapReplacementLetter = new LinkedHashMap<>();
 
     public static Integer countOfAllSymbols = 0;
+
+    static {
+        mapFrequencyOfLetters.put("о", 0.10983);
+        mapFrequencyOfLetters.put("е", 0.08483);
+        mapFrequencyOfLetters.put("а", 0.07998);
+        mapFrequencyOfLetters.put("и", 0.07367);
+        mapFrequencyOfLetters.put("н", 0.06700);
+        mapFrequencyOfLetters.put("т", 0.06318);
+        mapFrequencyOfLetters.put("с", 0.05473);
+        mapFrequencyOfLetters.put("р", 0.04746);
+        mapFrequencyOfLetters.put("в", 0.04533);
+        mapFrequencyOfLetters.put("л", 0.04343);
+        mapFrequencyOfLetters.put("к", 0.03486);
+        mapFrequencyOfLetters.put("м", 0.03203);
+        mapFrequencyOfLetters.put("д", 0.02977);
+        mapFrequencyOfLetters.put("п", 0.02804);
+        mapFrequencyOfLetters.put("у", 0.02615);
+        mapFrequencyOfLetters.put("я", 0.02001);
+        mapFrequencyOfLetters.put("ы", 0.01898);
+        mapFrequencyOfLetters.put("ь", 0.01735);
+        mapFrequencyOfLetters.put("г", 0.01687);
+        mapFrequencyOfLetters.put("з", 0.01641);
+        mapFrequencyOfLetters.put("б", 0.01592);
+        mapFrequencyOfLetters.put("ч", 0.01450);
+        mapFrequencyOfLetters.put("й", 0.01208);
+        mapFrequencyOfLetters.put("х", 0.00966);
+        mapFrequencyOfLetters.put("ж", 0.00940);
+        mapFrequencyOfLetters.put("ш", 0.00718);
+        mapFrequencyOfLetters.put("ю", 0.00639);
+        mapFrequencyOfLetters.put("ц", 0.00486);
+        mapFrequencyOfLetters.put("щ", 0.00361);
+        mapFrequencyOfLetters.put("э", 0.00331);
+        mapFrequencyOfLetters.put("ф", 0.00267);
+        mapFrequencyOfLetters.put("ъ", 0.00037);
+        mapFrequencyOfLetters.put("ё", 0.00013);
+    }
 
     public static String encryptToCaesarCode(String message, String qword, Integer key) {
 
@@ -39,7 +75,7 @@ public class Start {
         //Формируем результат
         String result = "";
         for(int i=0; i<message.length(); i++) {
-            if(ALPH.indexOf(message.charAt(i))==-1) {
+            if(ALPH.indexOf(Character.toLowerCase(message.charAt(i)))==-1) {
                 result = result + message.charAt(i);
             }
             else {
@@ -75,18 +111,19 @@ public class Start {
             }
         }
 
-        for(Map.Entry<String, Integer> entryOfMapCountOfSymbol : mapCountOfSymbol.entrySet()) {
-            double minDifference = 100.0;
-            String key = "";
-            for(Map.Entry<String, Double> entryOfMapFrequencyOfLetters : mapFrequencyOfLetters.entrySet()) {
-                double check = entryOfMapCountOfSymbol.getValue()/(countOfAllSymbols+0.0);
-                double difference = Math.abs(entryOfMapFrequencyOfLetters.getValue()-(entryOfMapCountOfSymbol.getValue()/(countOfAllSymbols+0.0)));
-                if(difference<minDifference && !mapReplacementLetter.containsValue(entryOfMapFrequencyOfLetters.getKey())) {
-                    minDifference = difference;
-                    key = entryOfMapFrequencyOfLetters.getKey();
-                }
-            }
-            mapReplacementLetter.put(entryOfMapCountOfSymbol.getKey(), key);
+        Map<String, Integer> map = new LinkedHashMap<>();
+        mapCountOfSymbol.entrySet().stream()
+                            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                            .forEach(entry -> map.put(entry.getKey(), entry.getValue()));
+
+        mapCountOfSymbol.clear();
+        mapCountOfSymbol.putAll(map);
+
+        String[] arrKeysByMapCountOfSymbol = (String[]) map.keySet().toArray(new String[map.size()]);
+        String[] arrKeysByMapFrequencyOfLetters = (String[]) mapFrequencyOfLetters.keySet().toArray(new String[map.size()]);
+
+        for(int i=0; i<ALPH.length(); i++) {
+            mapReplacementLetter.put(arrKeysByMapCountOfSymbol[i], arrKeysByMapFrequencyOfLetters[i]);
         }
 
         String result = "";
@@ -113,8 +150,8 @@ public class Start {
         BufferedOutputStream bufferedOutputStream1 = new BufferedOutputStream(fileOutputStream1);
         bufferedOutputStream1.write(encode.getBytes());
 
-        fileInputStream1.close();
-        fileOutputStream1.close();
+        bufferedOutputStream1.close();
+        bufferedInputStream1.close();
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -123,14 +160,18 @@ public class Start {
 
         byte[] bytesArrayFromChapter10Encode = bufferedInputStream2.readAllBytes();
         String stringFromChapter10Encode = new String(bytesArrayFromChapter10Encode, "UTF-8");
-        String decode = decodeByFrequencyAnalysis(stringFromChapter10);
+        String decode = decodeByFrequencyAnalysis(stringFromChapter10Encode);
 
         FileOutputStream fileOutputStream2 = new FileOutputStream("resources/Chapter10Decode.txt");
         BufferedOutputStream bufferedOutputStream2 = new BufferedOutputStream(fileOutputStream2);
         bufferedOutputStream2.write(decode.getBytes());
 
-        fileInputStream2.close();
-        fileOutputStream2.close();
+        bufferedOutputStream2.close();
+        bufferedInputStream2.close();
 
+        System.out.println(mapFrequencyOfLetters);
+        System.out.println(mapCountOfSymbol);
+        System.out.println(countOfAllSymbols);
+        System.out.println(mapReplacementLetter);
     }
 }
